1744a9af5973556d45c51b7a195e26cee96f89ff
Piotr Siergiej <129594776+dredd85@users.noreply.github.com>
2023-05-06 10:36:41 +0200

    Update 2.Api_Woo.py
    Added:
    - user input
    - sql table deletion on execute
    - a while loop to fetch products that are out of stock
    - some minor code changes to make it more readable

--- 2.Api_Woo.py
+++ 2.Api_Woo.py
@@ -3,27 +3,32 @@ import json
 import sqlite3
 
 wcapi = API(
-    url="https://armatura-sanitarna.com/",
-    consumer_key= input('Insert consumer key:'),
-    consumer_secret= input('Insert secret key:'),
+    url=input('Insert Woocommerce site:'),
+    consumer_key=input('Insert consumer key:'),
+    consumer_secret=input('Insert secret key:'),
     version="wc/v3",
-    timeout = 10
+    timeout=10
 )
-page = 1
-prod_count = 1
-
 conn = sqlite3.connect('DB_compare.db')
 cur = conn.cursor()
 
+# Deleting existing rows
+cur.execute('DELETE FROM prod_woo')
+conn.commit()
+
+page = 1
+product_count = 0
+
+# Fetching in stock products
 while True:
     products = wcapi.get('products', params={'per_page': 50, 'stock_status': 'instock', 'page': page}).json()
     
     if len(products) == 0: # no more products
-        print('All products fetched')
+        print('Products in stock fetched')
         break   
     for item in products:
-        prod_count += 1
-        print(prod_count)
+        product_count += 1
+        print(product_count)
         print('ID:', item['id'])
         print('Nazwa:', item['name'])
         print('Kod:', item['sku'])
@@ -32,6 +37,30 @@ while True:
                     VALUES (?, ?, ?)''', (item['id'], item['sku'], item['name']))
         conn.commit()
         
-    page += 1
+    page = page + 1
     print(page)
+
+page = 1
+
+# Fetching out of stock products
+while True:
+    products = wcapi.get('products', params={'per_page': 50, 'stock_status': 'outofstock', 'page': page}).json()
+    
+    if len(products) == 0: # no more products
+        print('Products out of stock fetched')
+        break   
+    for item in products:
+        product_count += 1
+        print(product_count)
+        print('ID:', item['id'])
+        print('Nazwa:', item['name'])
+        print('Kod:', item['sku'])
+        print('Stan:', item['stock_status'])
+        cur.execute('''INSERT OR IGNORE INTO prod_woo (id, kod_Dost, nazwa)
+                    VALUES (?, ?, ?)''', (item['id'], item['sku'], item['name']))
+        conn.commit()
+        
+    page = page + 1
+    print(page)
+
 cur.close()

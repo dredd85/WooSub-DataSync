5bb3f1cd1a03bc9fd12aedfd50514daffe815e84
dredd85 <piotr.siergiej@gmail.com>
2023-05-20 11:25:11 +0200

    Added new SQL querry with products stocks

--- 1.SQL_Subiekt.py
+++ 1.SQL_Subiekt.py
@@ -3,11 +3,11 @@ import sqlite3
 import pandas as pd
 
 # Connect to SQL Server database and execute a query
-server = input("Insert server name:")
-database = input("Insert database name:")
-username = input("Insert login:")
-password = input("Insert password:")
-query = "select distinct tw_id, tw_DostSymbol, tw_Nazwa, tw_StanMin, cht_IdCecha from dbo.tw__Towar inner join dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id inner join dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar where tw_StanMin != 0 and cht_IdCecha = 8;"
+server = input("Insert server name: ")
+database = input("Insert database name: ")
+username = input("Insert login: ")
+password = input("Insert password: ")
+query = "SELECT dbo.tw__Towar.tw_Nazwa as Nazwa, dbo.tw__Towar.tw_DostSymbol as Symbol,sum(dbo.tw__Towar.tw_StanMin) as Stan_Minimalny, sum(dbo.tw_Stan.st_Stan) as Stan FROM dbo.tw__Towar JOIN dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id JOIN dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar WHERE tw_StanMin != 0 and cht_IdCecha = 8 GROUP by dbo.tw__Towar.tw_Nazwa, dbo.tw__Towar.tw_DostSymbol;"
 cnxn_str = f"DRIVER=SQL Server Native Client 11.0;SERVER={server};DATABASE={database};UID={username};PWD={password}"
 cnxn = pyodbc.connect(cnxn_str)
 df_sqlserver = pd.read_sql(query, cnxn)

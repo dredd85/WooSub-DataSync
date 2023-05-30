29cb3a162704f70047f5462f9eebce538881c4fe
dredd85 <piotr.siergiej@gmail.com>
2023-05-30 09:58:09 +0200

    Fixed minor bug with counting minimal stock in SQL query

--- 1.SQL_Subiekt.py
+++ 1.SQL_Subiekt.py
@@ -7,7 +7,7 @@ server = input("Insert server name: ")
 database = input("Insert database name: ")
 username = input("Insert login: ")
 password = input("Insert password: ")
-query = "SELECT dbo.tw__Towar.tw_Nazwa as Nazwa, dbo.tw__Towar.tw_DostSymbol as Symbol,sum(dbo.tw__Towar.tw_StanMin) as Stan_Minimalny, sum(dbo.tw_Stan.st_Stan) as Stan FROM dbo.tw__Towar JOIN dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id JOIN dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar WHERE tw_StanMin != 0 and cht_IdCecha = 8 GROUP by dbo.tw__Towar.tw_Nazwa, dbo.tw__Towar.tw_DostSymbol;"
+query = "SELECT dbo.tw__Towar.tw_Nazwa as Nazwa, dbo.tw__Towar.tw_DostSymbol as Symbol, sum(dbo.tw__Towar.tw_StanMin)/2 as Stan_Minimalny, sum(dbo.tw_Stan.st_Stan) as Stan FROM dbo.tw__Towar JOIN dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id JOIN dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar WHERE tw_StanMin != 0 and cht_IdCecha = 8 GROUP by dbo.tw__Towar.tw_Nazwa, dbo.tw__Towar.tw_DostSymbol;"
 cnxn_str = f"DRIVER=SQL Server Native Client 11.0;SERVER={server};DATABASE={database};UID={username};PWD={password}"
 cnxn = pyodbc.connect(cnxn_str)
 df_sqlserver = pd.read_sql(query, cnxn)

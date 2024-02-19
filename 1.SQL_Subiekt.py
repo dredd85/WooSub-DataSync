1744dd638d5c1938a87128e12747cd149c96be40
dredd85 <piotr.siergiej@gmail.com>
2024-02-19 18:56:10 +0100

    Changed the querry to fetch data only from one base

--- 1.SQL_Subiekt.py
+++ 1.SQL_Subiekt.py
@@ -37,7 +37,7 @@ else:
 
 # Connect to SQL Server database and execute a query
 
-query = "SELECT dbo.tw__Towar.tw_Nazwa as Nazwa, dbo.tw__Towar.tw_DostSymbol as Symbol, sum(dbo.tw__Towar.tw_StanMin)/2 as Stan_Minimalny, sum(dbo.tw_Stan.st_Stan) as Stan FROM dbo.tw__Towar JOIN dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id JOIN dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar WHERE tw_StanMin != 0 and cht_IdCecha = 8 GROUP by dbo.tw__Towar.tw_Nazwa, dbo.tw__Towar.tw_DostSymbol;"
+query = "SELECT dbo.tw__Towar.tw_Nazwa as Nazwa, dbo.tw__Towar.tw_DostSymbol as Symbol, dbo.tw__Towar.tw_StanMin as Stan_Minimalny, dbo.tw_Stan.st_Stan as Stan FROM dbo.tw__Towar JOIN dbo.tw_Stan on dbo.tw_Stan.st_Towid = dbo.tw__Towar.tw_id JOIN dbo.tw_CechaTw on dbo.tw__Towar.tw_id = dbo.tw_CechaTw.cht_IdTowar WHERE tw_StanMin != 0 and cht_IdCecha = 8 and st_MagId = 4;"
 cnxn_str = f"DRIVER=SQL Server Native Client 11.0;SERVER={server};DATABASE={database};UID={username};PWD={password}"
 
 try:

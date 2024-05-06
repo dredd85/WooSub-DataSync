140061e4d55e868c8af41bb6237774dd537282c5
dredd85 <piotr.siergiej@gmail.com>
2024-05-06 20:26:30 +0200

    Added logic to check if there are products for update

--- 3.1.Auto_Update.py
+++ 3.1.Auto_Update.py
@@ -10,7 +10,23 @@ def panda_querry(querry):
     return sql
 
 out_of_stock_local = panda_querry("""
-SELECT * FROM prod_subiekt
+SELECT Symbol FROM prod_subiekt
 WHERE Stan_Minimalny > Stan""")
 
-print(out_of_stock_local)
+woo_products = panda_querry("""
+SELECT Symbol FROM prod_woo
+WHERE Stan > 0""")
+
+# Convert 'Symbol' columns to string type in both DataFrames
+out_of_stock_local['Symbol'] = out_of_stock_local['Symbol'].astype(str)
+woo_products['Symbol'] = woo_products['Symbol'].astype(str)
+
+products_to_update = []
+
+# Iterate through products from the website
+for index, product in woo_products.iterrows():
+    sku = product['Symbol']
+    # Check if SKU exists in the products with local stock lower than minimal
+    if sku in out_of_stock_local['Symbol'].values:
+        products_to_update.append(sku)
+        

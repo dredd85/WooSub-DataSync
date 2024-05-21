acd26f338d57355513066a6a2d9f7f8686330185
dredd85 <piotr.siergiej@gmail.com>
2024-05-21 19:02:35 +0200

    Bug in auto update, wrong product fetched. Next steps fetch ID

--- 3.1.Auto_Update.py
+++ 3.1.Auto_Update.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+from woocommerce import API
 
 conn = sqlite3.connect("DB_compare.db")
 cursor = conn.cursor()
@@ -31,5 +32,56 @@ for index, product in woo_products.iterrows():
     # Check if SKU exists in the products with local stock lower than minimal
     if sku in out_of_stock_local['Symbol'].values:
         products_to_update.append(sku)
-        
-print(products_to_update)
+    
+if not products_to_update:
+    print('No products to update')
+    exit()
+else:
+    print(f'Products to update: {products_to_update}')
+    user_decision = input('Proceed? Y/N: ').strip().lower()
+    if user_decision == "y":
+        print('Update in progress...')
+    else:
+        quit()
+    
+# WooCommerce API credentials
+url = 'https://armatura-sanitarna.com/'
+consumer_key = 'ck_0a4dc31230734eb0227ca96c8eb224a1d4614edf'
+consumer_secret = 'cs_b3cb8c361d8a075d6437055400adedc4cbc2e0f1'
+
+wcapi = API(url, consumer_key, consumer_secret, version="wc/v3", timeout=10)
+
+def get_product_id_by_sku(wcapi, sku):
+    try:
+        response = wcapi.get('products', params={'sku': sku}).json()
+        if response:
+            product_id = response[0]['id']
+            product_name = response[0]['name']
+            print(f"Found product: ID={product_id}, Name={product_name}, SKU={sku}")
+            return product_id, product_name  # Return both ID and Name
+        else:
+            print(f"No product found with SKU {sku}")
+            return None
+    except Exception as e:
+        print(f"Error fetching product ID for SKU {sku}: {e}")
+        return None
+
+# Fetch product ID based on SKU
+product_id = get_product_id_by_sku(wcapi, sku)
+
+print(product_id)
+'''
+if product_id is not None:
+    # Update product stock level using the fetched product ID
+    try:
+        product_data = {'stock_quantity': new_stock_level}
+        response = wcapi.put(f"products/{product_id}", data=product_data).json()
+        if 'message' in response:
+            print(f"Error updating stock level for product with SKU {sku}: {response['message']}")
+        else:
+            print(f"Stock level updated successfully for product with SKU {sku}")
+    except Exception as e:
+        print(f"Error updating stock level for product with SKU {sku}: {e}")
+else:
+    print("No product ID found for the given SKU, unable to update stock level.")
+'''
